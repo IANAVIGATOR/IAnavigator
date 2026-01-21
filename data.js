36a281762d1f5bfeb7f8be0f7d7ffd35d148aaba
IANAVIGATOR <iarandomnavigator@gmail.com>
2026-01-21 11:18:32 -0300

    Remove 'Flowith.IA' entry from data.js
    Removed 'Flowith.IA' entry and adjusted formatting.

--- data.js
+++ data.js
@@ -540,13 +540,7 @@ class DataManager {
                 id: this.generateId(),
                 name: 'Antigravity.IA',
                 url: 'https://antigravity.google/',
-                createdAt: Date.now()
-            },
-            {
-                id: this.generateId(),
-                name: 'Flowith.IA',
-                url: 'https://flowith.io/blank',
-                createdAt: Date.now()
+                createdAt: Date.now()           
             },
             {
                 id: this.generateId(),
