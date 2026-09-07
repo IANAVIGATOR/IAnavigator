bd7d67aa50bc6a6702c5194044ef5ba29eaba2a6
IANAVIGATOR <iarandomnavigator@gmail.com>
2026-09-07 18:50:35 -0300

    Fix formatting in data.js for createdAt property

--- data.js
+++ data.js
@@ -654,7 +654,13 @@ class DataManager {
                 id: this.generateId(),
                 name: 'Grammarly.IA',
                 url: 'https://www.grammarly.com/',
-                createdAt: Date.now()       
+                createdAt: Date.now()
+            },
+            {
+                id: this.generateId(),
+                name: 'Grammarly.IA',
+                url: 'https://www.happyhorse.com/',
+                createdAt: Date.now()                     
             }
         ];
     }
